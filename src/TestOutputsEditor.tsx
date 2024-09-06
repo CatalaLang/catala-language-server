882c6bf2351b7ebab0fa036a04fbbca70b38613f
PRIMET Romain <romain.primet@inria.fr>
2024-09-06 14:21:52 +0200

    more comments on the outputs/asserts editor

--- src/TestOutputsEditor.tsx
+++ src/TestOutputsEditor.tsx
@@ -10,10 +10,16 @@ import type { ReactElement } from 'react';
    - `test_outputs` (which may be better named `test_assertions`?) is the set of
    checked assertions; the names in `test_outputs` is/should be a subset of `tested_scope.outputs`
    - `test_outputs` is a named set of TestIo ; As far as I can tell, values should be mandatory
+     (although for records we should be able to test partial values, and for tuples and arrays, 
+      I do not know what the 'right' answer is).
 
  This component aggregates all outputs (assertions)
  for a single test and offers an interface for CRUD operations
  on assertions.
+
+ We will also need a 'reset assertions to current output value'
+ which will run the test and fill assertions values with the scope output.
+
  */
 export default function TestOutputsEditor(): ReactElement {
   return (
