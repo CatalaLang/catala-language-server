037792261755743c82f5b5ddbfe68530084c8961
PRIMET Romain <romain.primet@inria.fr>
2024-10-02 15:26:02 +0200

    update comment

--- src/TestOutputsEditor.tsx
+++ src/TestOutputsEditor.tsx
@@ -17,9 +17,8 @@ type Props = {
     (names and types, no value)
     - `test_outputs` (which may be better named `test_assertions`?) is the set of
     checked assertions; the names in `test_outputs` is/should be a subset of `tested_scope.outputs`
-    - `test_outputs` is a named set of TestIo ; As far as I can tell, values should be mandatory
-      (although for records we should be able to test partial values, and for tuples and arrays, 
-       I do not know what the 'right' answer is).
+    - `test_outputs` is a named set of TestIo ; Values are defined if an assertion is present,
+    and undefined otherwise.
 
    This component aggregates all outputs (assertions)
    for a single test and offers an interface for CRUD operations
