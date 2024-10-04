c56cd86723242816b8e03883f8668347f6676708
vbot <vincent.botbol@inria.fr>
2024-10-04 14:35:06 +0200

    display error message when catala-lsp not found

--- src/extension.ts
+++ src/extension.ts
@@ -1,35 +1,51 @@
-import { type ExtensionContext } from 'vscode';
+import * as vscode from 'vscode';
 import type { Executable } from 'vscode-languageclient/node';
 import { LanguageClient } from 'vscode-languageclient/node';
 import * as path from 'path';
 
 import { TestCaseEditorProvider } from './testCaseEditor';
 import { logger } from './logger';
+import * as fs from 'fs';
+import * as cmd_exists from 'command-exists';
 
 let client: LanguageClient;
 
-export function activate(context: ExtensionContext): void {
+export function activate(context: vscode.ExtensionContext): void {
   logger.show();
 
-  const cmd = context.asAbsolutePath(
+  const catala_binary = 'catala-lsp';
+
+  const local_path = context.asAbsolutePath(
     path.join('server', '_build', 'default', 'src', 'main.exe')
   );
 
-  const run: Executable = { command: cmd };
+  const is_binary_in_path = cmd_exists.sync(catala_binary);
+  const is_local_binary_present = fs.existsSync(local_path);
 
-  client = new LanguageClient(
-    cmd,
-    'Catala Language Server Protocol',
-    { run, debug: run },
-    {
-      documentSelector: [
-        { scheme: 'file', language: 'catala_en', pattern: '**/*.catala_en' },
-        { scheme: 'file', language: 'catala_fr', pattern: '**/*.catala_fr' },
-      ],
+  if (!is_binary_in_path && !is_local_binary_present) {
+    const err_msg =
+      'Catala LSP not found on the system: please refer to the installation procedure https://github.com/CatalaLang/catala-language-server/?tab=readme-ov-file#installation';
+    vscode.window.showErrorMessage(err_msg);
+  } else {
+    let cmd;
+    if (is_local_binary_present) {
+      cmd = local_path;
+    } else {
+      cmd = catala_binary;
     }
-  );
-
-  client.start();
+    const run: Executable = { command: cmd };
+    client = new LanguageClient(
+      cmd,
+      'Catala Language Server Protocol',
+      { run, debug: run },
+      {
+        documentSelector: [
+          { scheme: 'file', language: 'catala_en', pattern: '**/*.catala_en' },
+          { scheme: 'file', language: 'catala_fr', pattern: '**/*.catala_fr' },
+        ],
+      }
+    );
+  }
 
   // Always register the custom editor provider
   context.subscriptions.push(TestCaseEditorProvider.register(context));
