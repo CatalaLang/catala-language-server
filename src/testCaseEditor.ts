b4e387099c3f23deb3c4c62a8d9b9e1f220c1406
PRIMET Romain <romain.primet@inria.fr>
2024-10-11 11:57:14 +0200

    explain() call

--- src/testCaseEditor.ts
+++ src/testCaseEditor.ts
@@ -14,6 +14,7 @@ import {
   parseTestFile,
   atdToCatala,
   generate,
+  explainTestScope,
 } from './testCaseCompilerInterop';
 import { renameIfNeeded } from './testCaseUtils';
 
@@ -127,6 +128,9 @@ export class TestCaseEditorProvider implements vscode.CustomTextEditorProvider {
           break;
         }
         case 'TestExplainRequest': {
+          const { scope } = typed_msg.value;
+          const text = document.getText();
+          explainTestScope(text, scope);
           break;
         }
         case 'TestGenerateRequest': {
