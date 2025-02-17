d7c14216451cd13477b458f0eafb0791aa4dc067
PRIMET Romain <romain.primet@inria.fr>
2025-02-17 14:46:25 +0100

    update catala testcase read to use --buffer-path

--- src/testCaseEditor.ts
+++ src/testCaseEditor.ts
@@ -91,7 +91,11 @@ export class TestCaseEditorProvider implements vscode.CustomTextEditorProvider {
       const lang = getLanguageFromFileName(document.fileName);
       switch (typed_msg.kind) {
         case 'Ready': {
-          const parseResults = parseTestFile(document.getText(), lang);
+          const parseResults = parseTestFile(
+            document.getText(),
+            lang,
+            document.uri.fsPath
+          );
           logger.log(
             `Got ready message from webview, sending parsed document: \n ${JSON.stringify(parseResults)}`
           );
@@ -116,7 +120,11 @@ export class TestCaseEditorProvider implements vscode.CustomTextEditorProvider {
           vscode.workspace.applyEdit(edit).then(() => {
             postMessageToWebView({
               kind: 'Update',
-              value: parseTestFile(document.getText(), lang), //XXX concurrent edits?
+              value: parseTestFile(
+                document.getText(),
+                lang,
+                document.uri.fsPath
+              ), //XXX concurrent edits?
             });
           });
           break;
@@ -144,7 +152,11 @@ export class TestCaseEditorProvider implements vscode.CustomTextEditorProvider {
             //work for any struct defined in a module that needs to be
             //passed to the scope, for instance -- we need something better
             newTest.tested_scope.name = scopeUnderTest; //XXX
-            const currentTests = parseTestFile(document.getText(), lang);
+            const currentTests = parseTestFile(
+              document.getText(),
+              lang,
+              document.uri.fsPath
+            );
             if (currentTests.kind === 'Results') {
               newTest = renameIfNeeded(currentTests.value, newTest); //XXX kludge?
               const updatedTests = [...currentTests.value, newTest];
@@ -158,7 +170,11 @@ export class TestCaseEditorProvider implements vscode.CustomTextEditorProvider {
               vscode.workspace.applyEdit(edit).then(() => {
                 postMessageToWebView({
                   kind: 'Update',
-                  value: parseTestFile(document.getText(), lang),
+                  value: parseTestFile(
+                    document.getText(),
+                    lang,
+                    document.uri.fsPath
+                  ),
                 });
               });
             }
@@ -187,7 +203,11 @@ export class TestCaseEditorProvider implements vscode.CustomTextEditorProvider {
           const lang = getLanguageFromFileName(e.document.fileName);
           postMessageToWebView({
             kind: 'Update',
-            value: parseTestFile(e.document.getText(), lang),
+            value: parseTestFile(
+              e.document.getText(),
+              lang,
+              document.uri.fsPath
+            ),
           });
         }
       }
