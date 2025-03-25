4296fd4c1930404a6779cb4a4d601ec584455ffe
PRIMET Romain <romain.primet@inria.fr>
2025-03-25 13:31:21 +0100

    initial commit

--- src/uiEntryPoint.ts
+++ src/uiEntryPoint.ts
@@ -1,17 +1,26 @@
 import { createElement } from 'react';
 import { createRoot } from 'react-dom/client';
+import { IntlProvider } from 'react-intl';
 import TestFileEditor from './TestFileEditor';
 import { writeUpMessage } from './generated/test_case';
+import messages from './locales/en.json';
 
 const vscode = acquireVsCodeApi();
 
 export function renderUi(): void {
   const root = createRoot(document.getElementById('root') as HTMLElement);
   root.render(
-    createElement(TestFileEditor, {
-      contents: { state: 'initializing' },
-      vscode,
-    })
+    createElement(
+      IntlProvider,
+      {
+        locale: 'en',
+        messages: messages,
+      },
+      createElement(TestFileEditor, {
+        contents: { state: 'initializing' },
+        vscode: vscode,
+      })
+    )
   );
   vscode.postMessage(writeUpMessage({ kind: 'Ready' }));
 }
