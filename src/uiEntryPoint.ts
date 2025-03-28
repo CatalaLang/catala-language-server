8157290de3c5303884ed46d67818ecb1384730f1
PRIMET Romain <romain.primet@inria.fr>
2025-03-28 15:54:50 +0100

    revert unneeded changes in tsconfig

--- src/uiEntryPoint.ts
+++ src/uiEntryPoint.ts
@@ -7,8 +7,6 @@ export function renderUi(language: string): void {
   const vscode = acquireVsCodeApi();
 
   const root = createRoot(document.getElementById('root') as HTMLElement);
-  root.render(
-    createElement(App, {language, vscode})
-  );
+  root.render(createElement(App, { language, vscode }));
   vscode.postMessage(writeUpMessage({ kind: 'Ready' }));
 }
