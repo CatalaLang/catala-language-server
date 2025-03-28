508c55fe86a13db2a56fa3b87da72024e3babe33
PRIMET Romain <romain.primet@inria.fr>
2025-03-28 14:14:31 +0100

    First version of i18n, static imports

--- src/uiEntryPoint.ts
+++ src/uiEntryPoint.ts
@@ -1,26 +1,14 @@
 import { createElement } from 'react';
 import { createRoot } from 'react-dom/client';
-import { IntlProvider } from 'react-intl';
-import TestFileEditor from './TestFileEditor';
 import { writeUpMessage } from './generated/test_case';
-import messages from './locales/en.json';
+import App from './App';
 
-const vscode = acquireVsCodeApi();
+export function renderUi(language: string): void {
+  const vscode = acquireVsCodeApi();
 
-export function renderUi(): void {
   const root = createRoot(document.getElementById('root') as HTMLElement);
   root.render(
-    createElement(
-      IntlProvider,
-      {
-        locale: 'en',
-        messages: messages,
-      },
-      createElement(TestFileEditor, {
-        contents: { state: 'initializing' },
-        vscode: vscode,
-      })
-    )
+    createElement(App, {language, vscode})
   );
   vscode.postMessage(writeUpMessage({ kind: 'Ready' }));
 }
