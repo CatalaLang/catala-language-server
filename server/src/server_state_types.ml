070e030630b4476e8aa0f041b00b02545c265294
vbot <vincent.botbol@inria.fr>
2025-05-28 17:43:27 +0200

    Improve concurrency

--- server/src/server_state_types.ml
+++ server/src/server_state_types.ml
@@ -39,23 +39,113 @@ type 'a server_state = {
   documents : 'a document_state Doc_id.Map.t;
 }
 
+type 'result delayed_state =
+  | Ready of 'result server_state
+  | Delayed of {
+      doc_id : Doc_id.t;
+      curr_server_state : 'result server_state;
+      delayed_action : unit Lwt.t;
+      action : 'result server_state -> 'result server_state Lwt.t;
+    }
+
 type 'result locked_server_state = {
   lock : Lwt_mutex.t;
-  mutable state : 'result server_state;
+  mutable delayed_state : 'result delayed_state;
 }
 
 let use s (f : 'result server_state -> 'a Lwt.t) : 'a Lwt.t =
-  Lwt_mutex.with_lock s.lock @@ fun () -> f s.state
+  Lwt_mutex.with_lock s.lock
+  @@ fun () ->
+  match s.delayed_state with
+  | Ready state -> f state
+  | Delayed { doc_id = _; curr_server_state; delayed_action; action } ->
+    (* The delayed action is necessarily sleeping otherwise it wouldn't be
+       delayed so we can cancel the sleeping thread and force the computation *)
+    Lwt.cancel delayed_action;
+    Log.debug (fun m -> m "forcing delayed action");
+    let* server_state = action curr_server_state in
+    s.delayed_state <- Ready server_state;
+    f server_state
 
 let use_and_update
     s
     (f : 'result server_state -> ('a * 'result server_state) Lwt.t) : 'a Lwt.t =
   Lwt_mutex.with_lock s.lock
   @@ fun () ->
-  let* ret, new_state = f s.state in
-  s.state <- new_state;
+  let* ret, new_state =
+    match s.delayed_state with
+    | Ready state -> f state
+    | Delayed { doc_id = _; curr_server_state; delayed_action; action } ->
+      (* The delayed action is necessarily sleeping otherwise it wouldn't be
+         delayed so we can cancel the sleeping thread and force the
+         computation *)
+      Lwt.cancel delayed_action;
+      Log.debug (fun m -> m "forcing delayed action");
+      let* server_state = action curr_server_state in
+      f server_state
+  in
+  s.delayed_state <- Ready new_state;
   Lwt.return ret
+
+let make_delayed_action ?(delay = 1.) s action =
+  Log.debug (fun m -> m "start waiting on delayed action");
+  let* () = Lwt_unix.sleep delay in
+  Lwt_mutex.with_lock s.lock
+  @@ fun () ->
+  let curr_state =
+    match s.delayed_state with
+    | Ready _state -> assert false
+    | Delayed { curr_server_state = state; _ } -> state
+  in
+  Log.debug (fun m -> m "executing delayed action");
+  let* new_state = action curr_state in
+  s.delayed_state <- Ready new_state;
+  Lwt.return_unit
+
+let make_delayed_action ?delay s action =
+  let t = make_delayed_action ?delay s action in
+  Lwt.on_cancel t (fun () -> Log.debug (fun m -> m "canceling delayed action"));
+  t
+
+let delayed_update
+    ?delay
+    doc_id
+    s
+    (f : 'result server_state -> 'result server_state Lwt.t) : unit Lwt.t =
+  Lwt_mutex.with_lock s.lock
+  @@ fun () ->
+  match s.delayed_state with
+  | Ready state ->
+    let delayed_action = make_delayed_action ?delay s f in
+    s.delayed_state <-
+      Delayed { doc_id; curr_server_state = state; delayed_action; action = f };
+    Lwt.return_unit
+  | Delayed { doc_id = doc_id'; curr_server_state; delayed_action; action = _ }
+    when Doc_id.equal doc_id doc_id' ->
+    (* If both update are towards the same document, we cancel the existing
+       one *)
+    Lwt.cancel delayed_action;
+    let delayed_action = make_delayed_action ?delay s f in
+    s.delayed_state <-
+      Delayed { doc_id; curr_server_state; delayed_action; action = f };
+    Lwt.return_unit
+  | Delayed { doc_id = _; curr_server_state; delayed_action; action } ->
+    (* If the delayed update is one a different document, we wrap up the first
+       one before creating the new one *)
+    Lwt.cancel delayed_action;
+    Log.debug (fun m -> m "forcing delayed action");
+    let* new_server_state = action curr_server_state in
+    let delayed_action = make_delayed_action ?delay s f in
+    s.delayed_state <-
+      Delayed
+        {
+          doc_id;
+          curr_server_state = new_server_state;
+          delayed_action;
+          action = f;
+        };
+    Lwt.return_unit
 
 let make () =
   let state = { projects = Projects.empty; documents = Doc_id.Map.empty } in
-  { lock = Lwt_mutex.create (); state }
+  { lock = Lwt_mutex.create (); delayed_state = Ready state }
