70dfdd6e3d6bcc6ab2874b786f32b88c3faa55e7
Vincent Botbol <vincent.botbol@inria.fr>
2025-09-15 16:46:19 +0200

    Add configurations and commands for debugger

--- src/extension.ts
+++ src/extension.ts
@@ -10,11 +10,186 @@ import * as path from 'path';
 import { TestCaseEditorProvider } from './testCaseEditor';
 import { logger } from './logger';
 import * as fs from 'fs';
-import * as cmd_exists from 'command-exists';
+import cmd_exists from 'command-exists';
+import * as net from 'net';
+import { spawn } from 'child_process';
 
 let client: LanguageClient;
 
+function getCwd(bufferPath: string): string | undefined {
+  return vscode.workspace.getWorkspaceFolder(vscode.Uri.parse(bufferPath))?.uri
+    ?.fsPath;
+}
+
+function pathFromConfig(confId: string, defaultCmd: string): string {
+  const confPath = vscode.workspace
+    .getConfiguration('catala')
+    .get<string>(confId);
+  if (confPath === undefined || confPath === null || confPath.trim() === '')
+    return defaultCmd;
+  if (!fs.existsSync(confPath)) {
+    vscode.window.showWarningMessage(
+      `Could not find executable for ${confId} at ${confPath}, falling back to default`
+    );
+    return defaultCmd;
+  }
+  return confPath;
+}
+
+const clerkPath: string = pathFromConfig('clerkPath', 'clerk');
+
+interface IRunArgs {
+  uri: string;
+  scope: string;
+}
+
+async function selectScope(): Promise<IRunArgs | undefined> {
+  if (!client) {
+    vscode.window.showErrorMessage(
+      'Catala LSP is not running: cannot select a scope.'
+    );
+    return undefined;
+  }
+
+  const files_scopes_map: { path: string; scopes: string[] }[] =
+    await client.sendRequest(
+      'catala.getRunnableScopes',
+      vscode.workspace.getWorkspaceFolder
+    );
+  const possible_files: vscode.QuickPickItem[] = [
+    {
+      label: 'Catala source files',
+      kind: vscode.QuickPickItemKind.Separator,
+    },
+    ...files_scopes_map
+      .map((file) => {
+        return { label: file.path };
+      })
+      .sort((a, b) => a.label.localeCompare(b.label)),
+  ];
+
+  const file: vscode.QuickPickItem | undefined =
+    await vscode.window.showQuickPick(possible_files);
+  let possible_scopes: vscode.QuickPickItem[];
+  if (file) {
+    const file_scopes = files_scopes_map.find((f) => f.path == file.label);
+    if (file_scopes) {
+      possible_scopes = file_scopes.scopes.map((scope) => {
+        return { label: scope };
+      });
+      const scopes_to_choose: vscode.QuickPickItem[] = [
+        {
+          label: 'Catala scopes',
+          kind: vscode.QuickPickItemKind.Separator,
+        },
+        ...possible_scopes,
+      ];
+      const scope: vscode.QuickPickItem | undefined =
+        await vscode.window.showQuickPick(scopes_to_choose);
+      vscode.workspace.openTextDocument(vscode.Uri.file(file.label));
+      if (scope) return { uri: file.label, scope: scope.label };
+    }
+  }
+}
+
+async function runScope(): Promise<void> {
+  const args: IRunArgs | undefined = await selectScope();
+  if (args) {
+    const cwd = getCwd(args.uri);
+    const termName = `${args.scope} execution`;
+    let term = vscode.window.terminals.find((t) => t.name === termName);
+    if (!term) term = vscode.window.createTerminal({ name: termName, cwd });
+    term.show();
+    term.sendText(
+      [clerkPath, 'run', args.uri, '--scope', args.scope].join(' ')
+    );
+  }
+}
+vscode.commands.registerCommand('catala.run', runScope);
+
+async function debugScope(): Promise<void> {
+  const args: IRunArgs | undefined = await selectScope();
+  if (args) {
+    await vscode.debug.startDebugging(undefined, {
+      name: 'Run Catala program',
+      type: 'catala-debugger', // ou "extensionHost" si c'est pour développer l'extension
+      request: 'launch',
+      args: args,
+      stopOnEntry: true,
+    });
+  }
+}
+vscode.commands.registerCommand('catala.debug', debugScope);
+
 export function activate(context: vscode.ExtensionContext): void {
+  vscode.debug.registerDebugAdapterDescriptorFactory('catala-debugger', {
+    createDebugAdapterDescriptor(_session) {
+      const local_path = path.join(
+        '_build',
+        'default',
+        'server',
+        'src',
+        'main_dap.exe'
+      );
+      let dap_command;
+      if (fs.existsSync(local_path)) {
+        dap_command = context.asAbsolutePath(local_path);
+      } else if (cmd_exists.sync('catala-dap')) {
+        dap_command = 'catala-dap';
+      } else if (cmd_exists.sync('catala-dap.exe')) {
+        dap_command = 'catala-dap.exe';
+      } else {
+        vscode.window.showErrorMessage(
+          'Catala debugger not found on the system. Please refer to the installation procedure https://github.com/CatalaLang/catala-language-server/?tab=readme-ov-file#installation'
+        );
+        return undefined;
+      }
+      const server = net.createServer((socket) => {
+        const adapter = spawn(dap_command, []);
+        adapter.stdout.pipe(socket);
+        socket.pipe(adapter.stdin);
+        const output = vscode.window.createOutputChannel('Debugger Output');
+        adapter.stderr.on('data', (data: Buffer) => {
+          output.append(data.toString());
+        });
+      });
+
+      server.listen(0);
+      const port = (server.address() as net.AddressInfo).port;
+      return new vscode.DebugAdapterServer(port);
+    },
+  });
+
+  vscode.commands.registerCommand('catala.debugScope', async (args) => {
+    const file = args.uri;
+    const scope = args.scope;
+    const workspace = vscode.workspace.getWorkspaceFolder(
+      vscode.Uri.parse(file)
+    );
+    const config: vscode.DebugConfiguration = {
+      type: 'catala-debugger',
+      request: 'launch',
+      stopOnEntry: true,
+      name: `Debug: ${scope}`,
+      args: args,
+    };
+    const success = await vscode.debug.startDebugging(workspace, config);
+    if (!success) {
+      vscode.window.showErrorMessage('Fail to start a debugging session');
+    }
+  });
+
+  vscode.commands.registerCommand('catala.runScope', async (args) => {
+    const file = args.uri;
+    const scope = args.scope;
+    const cwd = getCwd(file);
+    const termName = `${scope} execution`;
+    let term = vscode.window.terminals.find((t) => t.name === termName);
+    if (!term) term = vscode.window.createTerminal({ name: termName, cwd });
+    term.show();
+    term.sendText([clerkPath, 'run', file, '--scope', scope].join(' '));
+  });
+
   const lsp_server_config_path = vscode.workspace
     .getConfiguration('catala')
     .get<string>('lspServerPath');
