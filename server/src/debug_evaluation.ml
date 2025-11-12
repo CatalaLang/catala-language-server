2f19fe9810fd4a8c072f407b2257ced4bec4e8a2
Vincent Botbol <vincent.botbol@inria.fr>
2025-11-12 18:06:04 +0100

    Adapt DAP to multi-errors changes

--- server/src/debug_evaluation.ml
+++ server/src/debug_evaluation.ml
@@ -88,6 +88,7 @@ let should_stop (e : dcalc_expr) =
   | EEmpty -> true
   | EErrorOnEmpty _ -> true
   | ECustom _ -> true
+  | EBad -> true
   | ELocation _ -> .
 
 let run_debugger
@@ -132,6 +133,7 @@ let run_debugger
     Lwt.catch
       (fun () ->
         let* r = Debug_interpret.interpret_with_env ~on_expr program scope in
+        Catala_utils.Message.report_delayed_errors_if_any ();
         Lwt.return_ok r)
       (fun exn -> Lwt.return_error exn)
   in
