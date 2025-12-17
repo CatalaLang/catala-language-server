c3766948399bba357ed0b3b7497ea94cbbb445a8
PRIMET Romain <romain.primet@inria.fr>
2025-12-17 10:16:33 +0100

    add regression test

--- tests/round_trip/round_trip.sh
+++ tests/round_trip/round_trip.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
+function cleanup(){
+    rm -f rename_to_typecheck.catala_en
+    rm -f to_typecheck.catala_en
+}
+
+trap cleanup EXIT
+
 # make round-trip (read then write)
 catala testcase read test_implicit_import.catala_en | catala testcase write --language en > to_typecheck.catala_en
 
 # ensure it typechecks
 clerk typecheck to_typecheck.catala_en || exit 1
+
+# generation + typecheck (regression test)
+catala testcase generate rename.catala_en --scope Example | catala testcase write --language en > rename_to_typecheck.catala_en
+clerk typecheck rename_to_typecheck.catala_en || exit 1
