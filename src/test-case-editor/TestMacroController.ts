23953043e32efcb6fda5332605737d1e6505f390
Arnaud Bihan <arnaud.bihan@functori.com>
2026-08-26 14:05:24 +0200

    Check undefined on expected just in case testResult was registered with an old version of the extension

--- src/test-case-editor/TestMacroController.ts
+++ src/test-case-editor/TestMacroController.ts
@@ -53,7 +53,10 @@ export class TestMacroController {
             index,
             filename: filename,
             test: e.entrypoint.value,
-            success: res.success && res.expected.length == 0,
+            success:
+              res.success && res.expected !== undefined
+                ? res.expected.length == 0
+                : true,
             date: res.date,
           };
           this.tests.push(testEntrypoint);
